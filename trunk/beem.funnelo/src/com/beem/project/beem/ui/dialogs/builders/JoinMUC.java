4053e954970fafced14274e088dfbcb5f7ad4f2d
luxmile@gmail.com <luxmile@gmail.com@0b0b2ff8-ffda-1718-f626-d86f2f15102e>
2012-02-03 14:20:14 +0000

    + MUC -> Chat Room

--- trunk/beem.funnelo/src/com/beem/project/beem/ui/dialogs/builders/JoinMUC.java
+++ trunk/beem.funnelo/src/com/beem/project/beem/ui/dialogs/builders/JoinMUC.java
@@ -40,7 +40,7 @@
     Flavien Astraud, November 26, 2009
     Head of the EIP Laboratory.
 
-*/
+ */
 package com.beem.project.beem.ui.dialogs.builders;
 
 import android.app.AlertDialog;
@@ -60,54 +60,56 @@ import com.beem.project.beem.ui.Chat;
  */
 public class JoinMUC extends AlertDialog.Builder {
 
-    private static final String TAG = "Dialogs.Builders > JoinMUC";
+	private static final String TAG = "Dialogs.Builders > JoinMUC";
 
-    private Context context ;
-    private EditText mEditTextRoom;
-    private EditText mEditTextPseudo;
-
-    /**
-     * Constructor.
-     * @param context context activity.
-     * @param roster Beem roster.
-     * @param contact the contact to modify.
-     */
-    public JoinMUC(final Context context) {
-	super(context);
-	this.context = context ;
-	LayoutInflater factory = LayoutInflater.from(context);
-	final View textEntryView = factory.inflate(
-	    R.layout.joinmucdialog, null);
-	setTitle("Join MUC");
-	setView(textEntryView);
-	mEditTextRoom = (EditText) textEntryView.findViewById(R.id.CDRoomDialogName);
-	mEditTextPseudo = (EditText) textEntryView.findViewById(R.id.CDNickDialogName);
-	setPositiveButton(R.string.OkButton, new DialogClickListener());
-	setNegativeButton(R.string.CancelButton, new DialogClickListener());
-    }
-
-    /**
-     * Event click listener.
-     */
-    class DialogClickListener implements DialogInterface.OnClickListener {
+	private Context context;
+	private EditText mEditTextRoom;
+	private EditText mEditTextPseudo;
 
 	/**
 	 * Constructor.
+	 *
+	 * @param context
+	 *            context activity.
+	 * @param roster
+	 *            Beem roster.
+	 * @param contact
+	 *            the contact to modify.
 	 */
-	public DialogClickListener() {
+	public JoinMUC(final Context context) {
+		super(context);
+		this.context = context;
+		LayoutInflater factory = LayoutInflater.from(context);
+		final View textEntryView = factory.inflate(R.layout.joinmucdialog, null);
+		setTitle("Join MUC");
+		setView(textEntryView);
+		mEditTextRoom = (EditText) textEntryView.findViewById(R.id.CDRoomDialogName);
+		mEditTextPseudo = (EditText) textEntryView.findViewById(R.id.CDNickDialogName);
+		setPositiveButton(R.string.OkButton, new DialogClickListener());
+		setNegativeButton(R.string.CancelButton, new DialogClickListener());
 	}
 
+	/**
+	 * Event click listener.
+	 */
+	class DialogClickListener implements DialogInterface.OnClickListener {
 
-	@Override
-	public void onClick(final DialogInterface dialog, final int which) {
-	    if (which == DialogInterface.BUTTON_POSITIVE) {
-	    	String room = mEditTextRoom.getText().toString();
-	    	String pseudo = mEditTextPseudo.getText().toString();
-		    Contact c = new Contact(room, true);
-		    Intent i = new Intent(context, Chat.class);
-		    i.setData(c.toUri(pseudo));
-		    context.startActivity(i);
-	    }
+		/**
+		 * Constructor.
+		 */
+		public DialogClickListener() {
+		}
+
+		@Override
+		public void onClick(final DialogInterface dialog, final int which) {
+			if (which == DialogInterface.BUTTON_POSITIVE) {
+				String room = mEditTextRoom.getText().toString();
+				String pseudo = mEditTextPseudo.getText().toString();
+				Contact c = new Contact(room, true);
+				Intent i = new Intent(context, Chat.class);
+				i.setData(c.toUri(pseudo));
+				context.startActivity(i);
+			}
+		}
 	}
-    }
 }
